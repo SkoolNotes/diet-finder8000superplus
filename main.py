6b4e301cfe4da928210f36b4198397dbe71a6ba5
Exr0n <mail@exr0n.com>
2020-09-03 11:56:51 -0700

    filter by what we want

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pandas
 
-PATH = 'nndb_flat.csv'
+DB_PATH = 'nndb_flat.csv' # 36 nutritional dimensions
 
+DIET_SET = [1001, 6194, 35056] # salted butter, chicken broth, raw seal meat
 
 def main():
-    f = pandas.read_csv(PATH)
-    print(f)
+    raw_data = pandas.read_csv(DB_PATH)
+    filtered = raw_data[raw_data.ID.isin(DIET_SET)]
+    print(filtered['ShortDescrip'])
 
 if __name__ == '__main__':
     main();
