6a8995f2176292972780142a7759d80a3984afcc
Exr0n <mail@exr0n.com>
2020-09-03 12:59:02 -0700

    framed as matrix equation for backprop?

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import pandas
+import pandas as pd
+import numpy  as np
 
-DB_PATH = 'nndb_flat.csv' # 36 nutritional dimensions
+DB_PATH = 'nndb_flat.csv' # 37 nutritional dimensions
 
 DIET_SET = [1001, 6194, 35056] # salted butter, chicken broth, raw seal meat
 
 def main():
-    raw_data = pandas.read_csv(DB_PATH)
-    filtered = raw_data[raw_data.ID.isin(DIET_SET)]
-    print(filtered['ShortDescrip'])
+    data = pd.read_csv(DB_PATH)
+    data = data[data.ID.isin(DIET_SET)]
+    data = data[data.columns[-37:]].to_numpy().transpose()
+    print(data)
 
 if __name__ == '__main__':
-    main();
+    main()
 
