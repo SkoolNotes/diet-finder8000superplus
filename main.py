fd2370d13c40b4e22f979d8449dcf439665c3f5e
Exr0n <mail@exr0n.com>
2020-09-03 18:19:36 -0700

    got amino acid values by food id

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy  as np
 import pprint
 
-import rdi
-
 config = {
     'paths': {
         'subsample_result': 'data/sub_sample_result.csv',
@@ -28,15 +26,12 @@ config = {
 
 def main():
     csvs = {
-        'subsample_result': pd.read_csv(config['paths']['subsample_result'], dtype={ 'nutrient_name': 'string' }),
-        # 'food_nutrient':    pd.read_csv(config['paths']['food_nutrient'], dtype={ 'id': int, 'fdc_id': int, 'nutrient_id': int, 'amount': float, 'data_points': int, 'derivation_id': int, 'min': float, 'max': float, 'median': float, 'footnote': 'string', 'min_year_acquired': int }),
+        'subsample_result': pd.read_csv(config['paths']['subsample_result'],
+            dtype={ 'nutrient_name': 'string' }),
         'food_nutrient':    pd.read_csv(config['paths']['food_nutrient'], low_memory=False),
         # 'food_name':        pd.read_csv(config['paths']['food_name'])
     }
-    # by_nutrient_source = {}
     csvs['food_nutrient'] = csvs['food_nutrient'].iloc[:, :4]
-
-    # print(csvs['food_nutrient'][csvs['food_nutrient']['id'] == 9638444])
 
     food = {}
     for nutrient in config['rdi']:
@@ -48,15 +43,9 @@ def main():
                 food[int(amount['fdc_id'])][nutrient] = float(amount['amount'])
             else:
                 food[int(amount['fdc_id'])] = {nutrient: float(amount['amount'])}
-        # by_nutrient_source[nutrient] = insts
 
-    # print(by_nutrient_source)
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(food)
-    # print(food)
-
-    # print(csvs['food_nutrient'])
-
 
 if __name__ == '__main__':
     main()
