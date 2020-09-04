4af5f82681158416c14f36ce630aa44b8e1893ca
zbuster05 <zakary.saya@gmail.com>
2020-09-03 18:39:33 -0700

    Fixed RDI to be dynamic

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pandas as pd
 import numpy  as np
 import pprint
 
+print("warning: pounds do not currently work")
+units = input("what units do you want to use? Type p for pounds and k for kilograms ")
+weight = 0
+if units == "p":
+    #do unit conversion stuff
+if units == "k"
+    weight = input("what is your weight in kilograms? ")
+
 config = {
     'paths': {
         'subsample_result': 'data/sub_sample_result.csv',
@@ -9,16 +17,16 @@ config = {
         'food_name': 'data/food.csv'
     },
     'rdi': {
-        'Phenylalanine': 33,
-        'Valine': 24,
-        'Leucine': 43,
-        'Isoleucine': 19,
-        'Lysine': 38,
-        'Threonine': 20,
-        'Tryptophan': 5,
-        'Methionine': 19,
-        'Histidine': 14,
-        'Protein': 0.8
+        'Phenylalanine': 33*weight,
+        'Valine': 24*weight,
+        'Leucine': 43*weight,
+        'Isoleucine': 19*weight,
+        'Lysine': 38*weight,
+        'Threonine': 20*weight,
+        'Tryptophan': 5*weight,
+        'Methionine': 19*weight,
+        'Histidine': 14*weight,
+        'Protein': 0.8*weight
     }
 }
 
@@ -50,7 +58,7 @@ def main():
 def olve(nm, rdi):
     output = np.linalg.lstsq(names, nm, rdi)[0]
     for i in range(len(names)):
-        print("You better freaking goddamn eat" + output[0][i][0] + "grams" + names[i])
+        print("You better freaking goddamn eat" + (output[0][i][0])*100 + "grams" + names[i])
     return output
 
     
